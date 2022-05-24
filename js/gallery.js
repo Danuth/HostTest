4ef25e02f13b6f2f2a2f1dc1389b3da80838c667
Danuth <pechdanuthd@gmail.com>
2022-05-24 09:00:36 +0700

    Added new files

---
+++ js/gallery.js
@@ -0,0 +1,130 @@
+/*(function () {
+    const customerImg = document.querySelector('#customer-item')//customer images
+    const customerProfile = document.querySelector('.customer-name')//customer name 
+    const customerComment = document.querySelector('#customer-comments')//customer comment 
+    const btn = document.querySelectorAll('.btn-review')
+
+    let i = 0 //store the first image index position as 0 
+    const generateCustomer = []//an empty array 
+
+    //for the list of each customers 
+    function Customers(image, profile, comment) { //parameter for Customers function 
+        this.image = image
+        this.profile = profile
+        this.comment = comment
+    }
+
+    //to generate new customer values 
+    function newCustomer(image, profile, comment) {
+        let Image = `images/customers/${image}.jpg` //to insert new customer image
+        let customerValue = new Customers(Image, profile, comment)
+        generateCustomer.push(customerValue)
+    }
+    //call the function 
+    newCustomer(0, 'Juliet Hobbs', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias fuga quideserunt ad,tempora ullam quisquam quaerat maiores modi sunt.1');
+    newCustomer(1, 'John Mcchallen', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias fuga quideserunt ad,tempora ullam quisquam quaerat maiores modi sunt.2')
+    newCustomer(2, 'Anne Janes', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias fuga quideserunt ad,tempora ullam quisquam quaerat maiores modi sunt.3')
+    newCustomer(3, 'Luke Rineer', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias fuga quideserunt ad,tempora ullam quisquam quaerat maiores modi sunt.4')
+    newCustomer(4, 'Bob Shea', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias fuga quideserunt ad,tempora ullam quisquam quaerat maiores modi sunt.5')
+    newCustomer(5, 'Michelle Hinsey', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias fuga quideserunt ad,tempora ullam quisquam quaerat maiores modi sunt.6')
+
+    //button function 
+    btn.forEach(function (button) {
+        button.addEventListener('click', function (e) {
+            if (e.target.parentElement.classList.contains('nextBtn')) {
+                i++//increment i by 1 if nextBtn is clicked 
+                if (i === generateCustomer.length) {
+                    i = 0
+                }
+                customerImg.src = generateCustomer[i].image
+                customerProfile.textContent = generateCustomer[i].profile
+                customerComment.textContent = generateCustomer[i].comment
+            }
+            if (e.target.parentElement.classList.contains('initialBtn')) { //if statement for initialBtn
+                if (i === 0) {
+                    i = generateCustomer.length
+                }
+                i-- //decrement the index by 1 if not 0 
+                customerImg.src = generateCustomer[i].image
+                customerProfile.textContent = generateCustomer[i].profile
+                customerComment.textContent = generateCustomer[i].comment
+            }
+        })
+    })
+})()
+*/
+const reviews = [
+    {
+        id: 1,
+        name: "Juliet Hobbs",
+        img:
+            "https://res.cloudinary.com/du0tega1b/image/upload/v1651308395/0_o5pe05.jpg",
+        text:
+            "OffWind does remarkably well in delivering lower-than-average prices, since the site is always aggregating the best options around.",
+    },
+    {
+        id: 2,
+        name: "Michelle Betts",
+        img:
+            "https://res.cloudinary.com/du0tega1b/image/upload/v1651308426/2_vvmrur.jpg",
+        text:
+            "The site has a reputation for reliability and is amazingly easy to use, thanks to its simple, clean interface.",
+    },
+    {
+        id: 3,
+        name: "John Shea",
+        img:
+            "https://res.cloudinary.com/du0tega1b/image/upload/v1651308406/1_wguorh.jpg",
+        text:
+            "Due to the rise of covid-19 cases, I had to cancel my holiday packages. I contacted the team and they were very cooperative and works so well with me. I highly recommended OffWind to everyone.",
+    },
+    {
+        id: 4,
+        name: "Luke Rineer ",
+        img:
+            "https://res.cloudinary.com/du0tega1b/image/upload/v1651308431/3_r79nxo.jpg",
+        text:
+            "I use OffWind ALL The Time! It is the BEST! I have never had any problems. I have never been disappointed! Keep Up the Great Customer Service!",
+    },
+];
+
+const img = document.getElementById("customer-item");
+const user = document.getElementById("profile");
+const comment = document.getElementById("comments");
+
+const initialBtn = document.querySelector('.initialBtn')
+const nextBtn = document.querySelector('.nextBtn')
+
+//initial item 
+let initialItem = 0;
+
+//load initial item 
+window.addEventListener('DOMContentLoaded', function () {
+    showUsers(initialItem);
+});
+
+//show person based on item 
+function showUsers(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    profile.textContent = item.name;
+    comments.textContent = item.text;
+}
+
+nextBtn.addEventListener('click', function () {
+    initialItem++;
+    if (initialItem > reviews.length - 1) {
+        initialItem = 0;
+    }
+    showUsers(initialItem);
+});
+
+//show previous item 
+initialBtn.addEventListener('click', function () {
+    initialItem--;
+    if (initialItem < 0) {
+        initialItem = reviews.length - 1;
+    }
+    showUsers(initialItem);
+});
+
---
+++ js/script.js
@@ -0,0 +1,96 @@
+var navbarDiv = document.querySelector('.navbar');
+const navbarCollapseDiv = document.getElementById('navbar-collapse');
+const navbarShowBtn = document.getElementById('navbar-show-btn');
+const navbarCloseBtn = document.getElementById('navbar-close-btn');
+
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        navbarDiv.classList.add('navbar-cng');
+    }
+    else {
+        navbarDiv.classList.remove('navbar-cng');
+    }
+});
+
+// show navbar
+navbarShowBtn.addEventListener('click', () => {
+    navbarCollapseDiv.classList.add('navbar-collapse-panel');
+});
+
+// close navbar
+navbarCloseBtn.addEventListener('click', () => {
+    navbarCollapseDiv.classList.remove('navbar-collapse-panel');
+});
+
+document.addEventListener('click', (e) => {
+    if (e.target.id != "navbar-collapse" && e.target.id != "navbar-show-btn" && e.target.parentElement.id != "navbar-show-btn") {
+        navbarCollapseDiv.classList.remove('navbar-collapse-panel');
+    }
+});
+
+// scroll up
+
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+
+    // scroll-up visible when the scroll is higher than 750vh (after the header)
+    if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
+        scrollUp.classList.add('show-scroll');
+    }
+    else {
+        scrollUp.classList.remove('show-scroll')
+    }
+}
+window.addEventListener('scroll', scrollUp)
+
+// dark mode
+
+var darkMode = document.getElementById('mode-btn');
+
+darkMode.onclick = function () {
+    document.body.classList.toggle('dark-mode');
+}
+
+// disable past dates
+
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+
+if (day < 10) {
+    day = '0' + day;
+}
+if (month < 10) {
+    month = '0' + month;
+}
+
+var minDate = year + '-' + month + '-' + day;
+
+document.getElementById('date-picker').setAttribute('min', minDate);
+document.getElementById('date-picker2').setAttribute('min', minDate);
+
+// popup
+var popup = document.getElementById('popup');
+
+document.querySelector('#open-popup-btn').onclick = () => {
+    popup.classList.toggle('open-popup');
+}
+
+document.querySelector('#close-popup-btn').onclick = () => {
+    popup.classList.remove('open-popup');
+}
+
+// login
+
+var loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () => {
+    loginForm.classList.remove('active');
+}
+
+
